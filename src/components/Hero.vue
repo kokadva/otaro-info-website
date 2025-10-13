7f13afdadccf3c64ed511a9f99d7ae0baf442a38
kokadva <cdval13@freeuni.edu.ge>
2025-10-13 19:12:55 +0400

    Updated Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -17,7 +17,7 @@
       <svg class="floating-svg svg-1" viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg">
         <path d="M10,50 Q30,20 50,50 T90,50 T130,50 T170,50" fill="none" stroke="#6bb896" stroke-width="15" stroke-linecap="round" opacity="0.45"/>
       </svg>
-      <svg class="floating-svg svg-2" viewBox="0 0 280 90" xmlns="http://www.w3.org/2000/svg">
+      <svg class="floating-svg svg-2" viewBox="0 0 280 90" opacity="0.4" xmlns="http://www.w3.org/2000/svg">
         <ellipse cx="70" cy="45" rx="60" ry="35" fill="#f5c842" opacity="0.4"/>
         <ellipse cx="170" cy="45" rx="70" ry="35" fill="#f5c842" opacity="0.4"/>
         <ellipse cx="210" cy="45" rx="50" ry="30" fill="#f5c842" opacity="0.35"/>
